60b6fb05b13c8938f8eeaba26f22ebe387b5d591
smartwalle <smartwalle@gmail.com>
2023-06-27 16:02:46 +0800

    改版

--- server.go
+++ server.go
@@ -2,115 +2,81 @@ package sse
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
-	"sync"
 )
 
 var ErrUnsupported = errors.New("server-Send events unsupported")
 
+type Handler func(stream *Stream)
+
 type Server struct {
-	mu      sync.Mutex
-	streams map[string]*Stream
+	onOpen  Handler
+	onClose Handler
 }
 
 func New() *Server {
 	var nServer = &Server{}
-	nServer.streams = make(map[string]*Stream)
+	nServer.onOpen = func(stream *Stream) {
+		panic(`open stream handler should be specified.
+var s = sse.Server()
+s.OnStreamOpen(func(stream *sse.Stream) {
+})`)
+	}
 	return nServer
 }
 
-func (this *Server) Serve(id, tag string, writer http.ResponseWriter, request *http.Request) error {
+func (this *Server) OnStreamOpen(handler Handler) {
+	this.onOpen = handler
+}
+
+func (this *Server) OnStreamClose(handler Handler) {
+	this.onClose = handler
+}
+
+func (this *Server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	var flusher, ok = writer.(http.Flusher)
 	if !ok {
-		return ErrUnsupported
+		fmt.Fprintln(writer, ErrUnsupported.Error())
+		return
 	}
 
 	writer.Header().Set("Content-Type", "text/events-stream")
 	writer.Header().Set("Cache-Control", "no-cache")
 	writer.Header().Set("Connection", "keep-alive")
 
-	this.mu.Lock()
-	var stream = this.streams[id]
-	if stream == nil {
-		stream = newStream(id)
-		this.streams[stream.id] = stream
-	}
-	this.mu.Unlock()
-
-	var subscriber = stream.addSubscriber(tag)
-	if subscriber == nil {
-		return ErrClosed
-	}
-
 	writer.WriteHeader(http.StatusOK)
 	flusher.Flush()
 
-	defer func() {
-		stream.removeSubscriber(subscriber)
+	var stream = newStream(request)
 
-		this.mu.Lock()
-		if stream.removable() {
-			delete(this.streams, stream.id)
-			stream.close()
+	var ready = make(chan struct{})
+
+	defer func() {
+		if this.onClose != nil {
+			<-ready
+			this.onClose(stream)
 		}
-		this.mu.Unlock()
+	}()
+
+	go func() {
+		this.onOpen(stream)
+		close(ready)
 	}()
 
 	for {
 		select {
 		case <-request.Context().Done():
-			return nil
-		case event := <-subscriber.events:
+			stream.Close()
+			return
+		case <-stream.closed:
+			return
+		case event := <-stream.events:
 			if event == nil {
-				return nil
+				return
 			}
 			writer.Write(Encode(event))
 			flusher.Flush()
 		}
 	}
-}
-
-func (this *Server) Close() error {
-	this.mu.Lock()
-	for id := range this.streams {
-		this.streams[id].close()
-		delete(this.streams, id)
-	}
-	this.mu.Unlock()
-	return nil
-}
-
-func (this *Server) StreamExists(id string) bool {
-	this.mu.Lock()
-	var _, ok = this.streams[id]
-	this.mu.Unlock()
-	return ok
-}
-
-func (this *Server) RemoveStream(id string) {
-	this.mu.Lock()
-	var stream = this.streams[id]
-	delete(this.streams, id)
-	this.mu.Unlock()
-
-	if stream != nil {
-		stream.close()
-	}
-}
-
-func (this *Server) Send(id string, event *Event) error {
-	this.mu.Lock()
-	var stream = this.streams[id]
-	this.mu.Unlock()
-
-	if stream == nil {
-		return ErrNotFound
-	}
-
-	select {
-	case <-stream.quit.Done():
-		return ErrClosed
-	case stream.events <- event:
-	}
-	return nil
 }
--- stream.go
+++ stream.go
@@ -2,113 +2,43 @@ package sse
 
 import (
 	"errors"
-	"github.com/smartwalle/nsync"
+	"net/http"
+	"sync"
 )
 
 var ErrClosed = errors.New("stream already closed")
-var ErrNotFound = errors.New("stream not found")
 
 type Stream struct {
-	id          string
-	quit        *nsync.Event
-	events      chan *Event
-	subscribers map[string]*Subscriber
-
-	register   chan *Subscriber
-	deregister chan *Subscriber
+	request   *http.Request
+	events    chan *Event
+	closed    chan struct{}
+	closeOnce sync.Once
 }
 
-func newStream(id string) *Stream {
+func newStream(req *http.Request) *Stream {
 	var nStream = &Stream{}
-	nStream.id = id
-	nStream.quit = nsync.NewEvent()
-	nStream.events = make(chan *Event)
-	nStream.subscribers = make(map[string]*Subscriber)
-	nStream.register = make(chan *Subscriber)
-	nStream.deregister = make(chan *Subscriber)
-	go nStream.run()
+	nStream.request = req
+	nStream.events = make(chan *Event, 8)
+	nStream.closed = make(chan struct{})
 	return nStream
 }
 
-func (this *Stream) Id() string {
-	return this.id
+func (this *Stream) Request() *http.Request {
+	return this.request
 }
 
-func (this *Stream) run() {
-	for {
-		select {
-		case subscriber := <-this.register:
-			if ele := this.subscribers[subscriber.tag]; ele != nil {
-				delete(this.subscribers, ele.tag)
-				ele.close()
-			}
-			this.subscribers[subscriber.tag] = subscriber
-		case subscriber := <-this.deregister:
-			if ele := this.subscribers[subscriber.tag]; ele == subscriber {
-				delete(this.subscribers, subscriber.tag)
-			}
-			subscriber.close()
-		case event := <-this.events:
-			for _, ele := range this.subscribers {
-				if event.Tag != "" && event.Tag != ele.tag {
-					continue
-				}
-
-				select {
-				case <-ele.quit.Done():
-					continue
-				case ele.events <- event:
-				}
-			}
-		case <-this.quit.Done():
-			for _, ele := range this.subscribers {
-				ele.close()
-			}
-			this.subscribers = nil
-			return
-		}
-	}
+func (this *Stream) Close() error {
+	this.closeOnce.Do(func() {
+		close(this.closed)
+	})
+	return nil
 }
 
-func (this *Stream) close() {
-	if this.quit.HasFired() {
-		return
-	}
-	this.quit.Fire()
-	close(this.events)
-	close(this.register)
-	close(this.deregister)
-}
-
-func (this *Stream) addSubscriber(tag string) *Subscriber {
-	var subscriber = newSubscriber(tag)
+func (this *Stream) Send(event *Event) error {
 	select {
-	case <-this.quit.Done():
+	case <-this.closed:
+		return ErrClosed
+	case this.events <- event:
 		return nil
-	case this.register <- subscriber:
-	}
-	return subscriber
-}
-
-func (this *Stream) removeSubscriber(subscriber *Subscriber) {
-	if subscriber != nil {
-		select {
-		case <-this.quit.Done():
-		case <-subscriber.quit.Done():
-		case this.deregister <- subscriber:
-			select {
-			case <-this.quit.Done():
-			case <-subscriber.quit.Done():
-			}
-		}
-	}
-}
-
-func (this *Stream) removable() bool {
-	select {
-	case <-this.quit.Done():
-		return true
-	default:
-		return len(this.subscribers) == 0
 	}
 }
